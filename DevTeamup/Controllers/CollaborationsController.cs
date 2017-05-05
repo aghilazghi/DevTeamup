0c059d8916f2b52aa26ee9933108fe6a7d6fd47a
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-05-05 13:31:45 -0400

    Build Collaborations Api

---
+++ DevTeamup/Controllers/CollaborationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DevTeamup.Dtos;
+using DevTeamup.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DevTeamup.Controllers
+{
+    public class CollaborationsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CollaborationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Join(CollaborationDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Collaborations.Any(a => a.ContributorId == userId && a.TeamupId == dto.TeamupId))
+                return BadRequest("It aready exists");
+
+            var collaboration = new Collaboration
+            {
+                TeamupId = dto.TeamupId,
+                ContributorId = userId
+            };
+
+            _context.Collaborations.Add(collaboration);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ DevTeamup/Dtos/CollaborationDto.cs
@@ -0,0 +1,7 @@
+namespace DevTeamup.Dtos
+{
+    public class CollaborationDto
+    {
+        public int TeamupId { get; set; }
+    }
+}
