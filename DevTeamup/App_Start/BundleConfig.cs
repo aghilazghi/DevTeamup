2b5ea43db1fade7236b8c6d4f3c224102d763804
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-05-11 17:15:06 -0400

    Consolidate bundles

--- DevTeamup/App_Start/BundleConfig.cs
+++ DevTeamup/App_Start/BundleConfig.cs
@@ -7,11 +7,14 @@ namespace DevTeamup
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore.js",
                         "~/Scripts/moment.js",
-                        "~/Scripts/toastr.min.js"));
+                        "~/Scripts/toastr.min.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +23,6 @@ namespace DevTeamup
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
